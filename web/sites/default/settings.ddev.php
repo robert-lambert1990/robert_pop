be12296bc1432d7dc303a143a0b89e1e4138d5dd
robertlambert-untitled <robert.lambert@un.titled.co.uk>
2025-04-25 13:26:31 +0100

    Drush and devel modules - setup configuration directories

--- web/sites/default/settings.ddev.php
+++ web/sites/default/settings.ddev.php
@@ -34,7 +34,7 @@ $settings['trusted_host_patterns'] = ['.*'];
 
 // Set $settings['config_sync_directory'] if not set in settings.php.
 if (empty($settings['config_sync_directory'])) {
-  $settings['config_sync_directory'] = 'sites/default/files/sync';
+  $settings['config_sync_directory'] = '../config/sync';
 }
 
 // Override drupal/symfony_mailer default config to use Mailpit.
